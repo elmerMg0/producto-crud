950b3876342f63177474a59d1123850d98e95509
elmerMg0 <mendozagutierrezelmer20@gmail.com>
2023-02-08 01:40:49 -0400

    api services inicializated

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,13 +1,52 @@
-import { useEffect } from "react"
-import {ApiServices} from '../services/api.services'
+import { useEffect, useState } from "react";
+import { ApiServices } from "../services/api.services";
 
-export default function Producto(){
+export default function Producto() {
+  const [products, setProducts] = useState([]);
+  const [infoProducts, setInfoProducts] = useState({})
+  const [pageNumber, setPageNumber] = useState(200)
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    useEffect(()=>{
-        ApiServices.getProductos()
-    }, [] )
+  useEffect( ()=>{
+    getProducts();
 
-    return (
-        <h1>Producto</h1>
-    )
+  }, [pageNumber])
+
+  function getProducts(){
+    ApiServices.getProductos( pageNumber )
+      .then((data) => {
+        setProducts(data.data);
+        setInfoProducts(data);
+      })
+  }
+
+  function updateProduct(){
+    ApiServices.updateProduct( { name: "Pepsi", descripcion: "Bebida de cola", precio: "1.5"} , 1)
+                .then((res)=> {
+                    console.log(res)
+                })
+  }
+
+  return (
+    <div>
+      {products && products.length > 0 ? (
+        products.map((product, index) => {
+          return (
+            <div key={index} style={ {border: "1px solid orange"} }>
+              <h5>{product.id}</h5>
+              <h5>{product.nombre}</h5>
+              <h5>{product.stock}</h5>
+            </div>
+          );
+        })
+      ) : (
+        <h5>No existen productos</h5>
+      )}
+      <button onClick={()=>setPageNumber( infoProducts.previus )}>Previus</button>
+      <button onClick={()=>setPageNumber( infoProducts.next )}>Next</button >
+      <button onClick={()=> updateProduct() }>Update</button >
+    </div>  
+  );
 }
--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,16 +1,35 @@
 const APIURL = process.env.REACT_APP_API_URL;
 
 export const ApiServices = {
-    getProductos : async () => {
-        let response = {} ;
-        await fetch(APIURL+"producto/ifndex").
+    getProductos : async ( pageNumber) => {
+        //let response = {} ;
+        try {
+          const response = await fetch(APIURL+"producto/index?page="+pageNumber)
+          const data = await response.json();
+          return data;
+          
+        } catch (error) {
+            console.error("Se produjo un error", error);
+        }
                                 
-        then( (res) => response = res.json())
-        .catch( error => console.error(error))
+      /*  then( (res) => response = res.json())
+         .catch( error => console.error(error)) */
+    },
+    updateProduct : async ( product, id ) => {
+      try {
+        const response = await fetch(APIURL+"producto/update?id="+id,{
+            method: "PUT",
+            headers: {
+              'content-type': "application/json"
+            },
+            body: JSON.stringify(product)
+        })
+        const data = await response.json();
+        return data;
         
-        
-      /*   const data = await response.json();
-        console.log(data); */
+      } catch (error) {
+          console.error("Se produjo un error", error);
+      }
     }
     ,
 }
